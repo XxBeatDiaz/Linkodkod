f1090092902205d7e471be43b6e821bd3dd8ae02
BeatDiaz <beatdiazmusic@gmail.com>
2025-09-08 17:14:13 +0300

    make clien dir and server dir

--- src/mock/postsMock.ts
+++ src/mock/postsMock.ts
@@ -1,7 +1,7 @@
 import type { PostType } from '../types/postType';
 
 //Fake posts 
-export const posts = [
+export const fakePosts = [
     {
         "id":1,
         "img": "../public/post.png",
@@ -21,13 +21,6 @@ export const posts = [
         "img": "../public/post.png",
         "description": "bibi sachi3",
         "authorName": "mali",
-        "timeStemp": "09/04/20"
-    },
-    {
-        "id":4,
-        "img": "../public/post.png",
-        "description": "bibi sachi4",
-        "authorName": "tami",
         "timeStemp": "09/04/20"
     }
 ] as PostType[];
